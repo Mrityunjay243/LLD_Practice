9e4dc6abb05908dcfe36094369f8977643f8a609
Mrityunjay243 <mrityunjayjoshi565@gmail.com>
2025-06-21 15:04:09 -0400

    Added logger header file

--- LoggingFramework/Logger.hpp
+++ LoggingFramework/Logger.hpp
@@ -9,7 +9,27 @@
 #include "LogAppender.hpp"
 
 class Logger {
+    private:
+        std::string name; 
+        LogLevel minLevel; 
+        std::vector<std::shared_ptr<LogAppender>> appenders; 
 
+    public:     
+        Logger(const std::string& name, LogLevel minlevel = LogLevel::INFO); 
+
+        void addAppender(std::shared_ptr<LogAppender> appender); 
+        void setMinLevel(LogLevel level); 
+
+        void log(LogLevel level, const std::string& message); 
+        void trance(const std::string& message); 
+        void debug(const std::string& message); 
+        void info(const std::string& message); 
+        void warn(const std::string& message); 
+        void error(const std::string& message); 
+        void fatal(const std::string& message); 
+
+    private:
+        bool isLevelEnabled(LogLevel level) const; 
 };
 
 #endif
