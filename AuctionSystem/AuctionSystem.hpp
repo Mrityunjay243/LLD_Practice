a4e4041fad3d2e9016382d2f138ce0648bb103df
Mrityunjay243 <mrityunjayjoshi565@gmail.com>
2025-07-08 18:37:17 -0400

    Added class declerations for AuctionSystem class

---
+++ AuctionSystem/AuctionSystem.hpp
@@ -0,0 +1,43 @@
+#ifndef AUCTION_SYSTEM_HPP
+#define AUCTION_SYSTEM_HPP
+
+#include <vector>
+#include <string>
+#include "User.hpp"
+#include "Item.hpp"
+#include "Auction.hpp"
+
+class AuctionSystem{
+    private: 
+        std::vector<User*> users; 
+        std::vector<Item*> items;
+        std::vector<Auction*> auctions; 
+        int itemIdCounter; 
+        int auctionIdCounter; 
+
+    public:
+        AuctionSystem(); 
+        ~AuctionSystem(); 
+
+        User* registerUser(std::string username, std::string email); 
+        Item* createItem(std::string sellerId, std::string name, std::string description, 
+                ItemCategory category, double startingPrice); 
+
+        Auction* createAuction(std::string itemId, std::time_t startTime, std::time_t endTime); 
+
+        bool placeBid(std::string auctionId, std::string bidderId, double amount); 
+        bool addUserBalance(std::string userId, double amount); 
+        void startAuction(std::string auctionId); 
+        void cancelAuction(std::string auctionId); 
+
+        std::vector<Auction*> getActiveAuctions() const; 
+        std::vector<Item*> searchItems() const; 
+
+        void displayUserInfo(std::string userId) const; 
+        void displayItemInfo(std::string itemId) const; 
+        void displayAuctionInfo(std::string auctionId) const; 
+        std::string generateItemId(); 
+        std::string generateAuctionId(); 
+};
+
+#endif
